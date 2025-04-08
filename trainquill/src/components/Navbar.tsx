6be69561673abda4ddc86e1328d89d7fe31e6f61
giahienpham <giahien14604@gmail.com>
2025-04-08 23:37:13 +0800

    add navbar

---
+++ trainquill/src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React, { useState } from "react";
+import logo from "../../public/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+
+const LINKS = [
+  { text: "Home", href: "/" },
+  { text: "BodyScan", href: "/bodyscan" },
+  { text: "Reframe", href: "/reframe" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full h-24 shadow-xl bg-white z-50">
+      <div className="flex items-center px-4 h-full w-full">
+        <Link href="/">
+          <img
+            src={logo.src}
+            alt="Logo"
+            width={90}
+            height={90}
+            className="cursor-pointer"
+          />
+        </Link>
+        <div>
+          <ul className="hidden sm:flex">
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                About us
+              </li>
+            </Link>
+            <Link href="/bodyscan">
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Bodyscan
+              </li>
+            </Link>
+            <Link href="/name">
+              <li className="ml-10 uppercase hover:border-b text-xl">Name</li>
+            </Link>
+            <Link href="/frame">
+              <li className="ml-10 uppercase hover:border-b text-xl">Frame</li>
+            </Link>
+            <Link href="/reframe">
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Reframe
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
